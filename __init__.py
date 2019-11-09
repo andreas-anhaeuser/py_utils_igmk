35d4ec6c512ec78a8976ea76888068aca80d4c4e
Andreas Anhaeuser <andreas.anhaeuser@posteo.net>
2019-11-09 15:03:22 +0200

    Initial commit or repeat_function_call

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ from . import datetime_utils
 from . import filelock
 from . import fluids
 from . import geometry
+# from . import geodata
 from . import humidity
 from . import iso
 from . import math
@@ -13,6 +14,7 @@ from . import mcs
 from . import meteo
 from . import netcdf
 from . import record_file
+from . import repeat_function_call
 from . import string_utils
 from . import sun
 from . import tables
