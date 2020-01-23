7e1843084527ea2e94b7167c7f57e958f8819ab1
Andreas Anhaeuser <andreas.anhaeuser@posteo.net>
2020-01-23 21:37:38 +0100

    Add ppb to ug converter

--- meteo_density.py
+++ meteo_density.py
@@ -1,9 +1,23 @@
+# PyPI modules
 import numpy as np
 
-_dobson_unit = 2.2687e20            # m-2
+# constants
 _avogadro_constant = 6.02214076e23  # mol-1
+_boltzmann_constant = 1.380649e-23  # (J K-1)
+_dobson_unit = 2.2687e20            # m-2
 
-def volume_to_mass_density(v, M, p=1013.25e2, T=288.15):
+# defaults
+_pressure = 1013.25e2   # N m-2
+_temperature = 288.15   # K
+
+def ppb_to_ug(ppb, substance, p=None, T=None):
+    M = molecular_mass(substance)
+    v = ppb * 1e-9
+    mass_si = volume_to_mass_density(v, M, p, T)
+    mass_ug = mass_si * 1e9
+    return mass_ug
+
+def volume_to_mass_density(v, M, p=None, T=None):
     """
 
         Parameters
@@ -22,8 +36,15 @@ def volume_to_mass_density(v, M, p=1013.25e2, T=288.15):
         rho : float
             (kg m-3) mass density
     """
-    kB = 1.380649e-23   # (J K-1) Boltzmann constant
-    rho = p * M / (T * kB) * v
+    # defaults ============================================
+    if p is None:
+        p = _pressure
+    if T is None:
+        T = _temperature
+    # =====================================================
+
+    rho = p * M / (T * _boltzmann_constant) * v
+
     return rho
 
 def si_to_dobson_unit(value, substance):
