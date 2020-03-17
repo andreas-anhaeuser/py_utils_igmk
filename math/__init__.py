1c95a7505a8cfae10794dac40f64812658d6007c
Andreas Anhaeuser <andreas.anhaeuser@posteo.net>
2020-03-17 09:41:15 +0100

    Some math tools

--- math/__init__.py
+++ math/__init__.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+from . import functions
 from . import largest_remainder
+from . import primes
 from . import range
 from . import round
+from . import running_mean
--- math/round.py
+++ math/round.py
@@ -4,9 +4,27 @@ import collections
 # PyPI modules
 import numpy as np
 
-def round_digits(x, digits=1):
+def round_digits(x, digits=1, method='round'):
+    """
+
+        Parameters
+        ----------
+        x : float
+        digits : int, optional
+            (default: 1) number of significant digits
+        method : {'round', 'floor', 'ceil'}, optional
+            (default: 'round')
+
+        Returns
+        -------
+        float
+    """
     if digits <= 0:
         raise ValueError('`digits` must be a positive integer.')
+
+    known_methods = ('round', 'floor', 'ceil')
+    if method not in known_methods:
+        raise ValueError('Unknown method: %s' % method)
 
     if isinstance(x, collections.Iterable):
         N = len(x)
@@ -18,4 +36,21 @@ def round_digits(x, digits=1):
     magnitude_fractional = np.log10(np.abs(x))
     magnitude = int(np.floor(magnitude_fractional))
     decimals = digits - 1 - magnitude
-    return np.round(x, decimals)
+    rounded = np.round(x, decimals)
+
+    if method == 'round':
+        return rounded
+
+    if method == 'floor' and rounded <= x:
+        return rounded
+
+    if method == 'ceil' and rounded >= x:
+        return rounded
+
+    correction = 10**(-decimals)
+
+    if method == 'floor':
+        return rounded - correction
+
+    if method == 'ceil':
+        return rounded + correction
