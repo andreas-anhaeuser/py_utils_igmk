da2a134e0550bf06221d25f8181553f5c2a34067
Andreas Anhaeuser <andreas.anhaeuser@posteo.net>
2021-03-26 21:33:46 +0100

    Minor style fixes

--- geodata/coordinates.py
+++ geodata/coordinates.py
@@ -1,5 +1,25 @@
+# PyPI
 import numpy as np
 import pyproj
+
+
+def compute_coordinates_from_transform(meta):
+    """Return x and y as two 1d-arrays."""
+    Nx = meta['width']
+    Ny = meta['height']
+    transform = meta['transform']
+
+    # i : indices in x-direction
+    # j : inideces in y-direction
+    i = np.arange(Nx)
+    j = np.arange(Ny)
+
+    # apply affine transformation from index to coordinate
+    # `x0` and `y0` remain unused
+    # (the zeros can be replace by any value without altering the output)
+    x, y0 = transform * (i, 0)
+    x0, y = transform * (0, j)
+    return x, y
 
 def get_coordinate_order():
     """Return 'lonlat' or 'latlon'."""
@@ -59,21 +79,3 @@ def get_coordinate_order():
         return old_order
 
     return new_order
-
-def compute_coordinates_from_transform(meta):
-    """Return x and y as two 1d-arrays."""
-    Nx = meta['width']
-    Ny = meta['height']
-    transform = meta['transform']
-
-    # i : indices in x-direction
-    # j : inideces in y-direction
-    i = np.arange(Nx)
-    j = np.arange(Ny)
-
-    # apply affine transformation from index to coordinate
-    # `x0` and `y0` remain unused
-    # (the zeros can be replace by any value without altering the output)
-    x, y0 = transform * (i, 0)
-    x0, y = transform * (0, j)
-    return x, y
